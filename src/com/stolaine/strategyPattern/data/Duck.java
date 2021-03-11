00a41f6bbb2dd0d117320fc0e8b8a12cdf11ea78
Sheshank Kumar <sheshank238mukul@gmail.com>
2021-03-11 17:22:52 +0530

    Change access modifier for member variables of Duck and add setter and getter.

--- src/com/stolaine/strategyPattern/data/Duck.java
+++ src/com/stolaine/strategyPattern/data/Duck.java
@@ -2,9 +2,9 @@ package com.stolaine.strategyPattern.data;
 
 public class Duck {
 
-    protected IQuackBehaviour quackBehaviour;
-    protected IDisplayBehaviour displayBehaviour;
-    protected IFlyBehaviour flyBehaviour;
+    private IQuackBehaviour quackBehaviour;
+    private IDisplayBehaviour displayBehaviour;
+    private IFlyBehaviour flyBehaviour;
 
     public Duck(IQuackBehaviour quackBehaviour, IDisplayBehaviour displayBehaviour, IFlyBehaviour flyBehaviour) {
         this.quackBehaviour = quackBehaviour;
@@ -22,5 +22,29 @@ public class Duck {
 
     public interface IDisplayBehaviour {
         void display();
+    }
+
+    public IQuackBehaviour getQuackBehaviour() {
+        return quackBehaviour;
+    }
+
+    public void setQuackBehaviour(IQuackBehaviour quackBehaviour) {
+        this.quackBehaviour = quackBehaviour;
+    }
+
+    public IDisplayBehaviour getDisplayBehaviour() {
+        return displayBehaviour;
+    }
+
+    public void setDisplayBehaviour(IDisplayBehaviour displayBehaviour) {
+        this.displayBehaviour = displayBehaviour;
+    }
+
+    public IFlyBehaviour getFlyBehaviour() {
+        return flyBehaviour;
+    }
+
+    public void setFlyBehaviour(IFlyBehaviour flyBehaviour) {
+        this.flyBehaviour = flyBehaviour;
     }
 }
