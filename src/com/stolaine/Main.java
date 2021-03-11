19996d8b0760fbba18fdaf62234cb1b9aedc0419
Sheshank Kumar <sheshank238mukul@gmail.com>
2021-03-11 17:23:43 +0530

    Create a Duck object and show its features.

--- src/com/stolaine/Main.java
+++ src/com/stolaine/Main.java
@@ -1,9 +1,16 @@
 package com.stolaine;
 
+import com.stolaine.strategyPattern.data.Duck;
+import com.stolaine.strategyPattern.displayFeatures.DisplayAsText;
+import com.stolaine.strategyPattern.flyFeatures.SimpleFly;
+import com.stolaine.strategyPattern.quackFeatures.SimpleQuack;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("First Commit.");
+        Duck duck = new Duck(new SimpleQuack(), new DisplayAsText(), new SimpleFly());
+        duck.getQuackBehaviour().quack();
+        duck.getDisplayBehaviour().display();
+        duck.getFlyBehaviour().fly();
     }
 }
