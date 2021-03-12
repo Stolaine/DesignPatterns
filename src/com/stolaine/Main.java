bc4b24c43005180bf78bd90713fb1e3bf9259a19
Sheshank Kumar <sheshank238mukul@gmail.com>
2021-03-12 14:22:28 +0530

    Write execution code for Observer Pattern.

--- src/com/stolaine/Main.java
+++ src/com/stolaine/Main.java
@@ -1,5 +1,8 @@
 package com.stolaine;
 
+import com.stolaine.observerPattern.observables.WeatherStation;
+import com.stolaine.observerPattern.observers.PhoneDisplay;
+import com.stolaine.observerPattern.observers.WindowDisplay;
 import com.stolaine.strategyPattern.data.Duck;
 import com.stolaine.strategyPattern.displayFeatures.DisplayAsText;
 import com.stolaine.strategyPattern.flyFeatures.SimpleFly;
@@ -8,9 +11,13 @@ import com.stolaine.strategyPattern.quackFeatures.SimpleQuack;
 public class Main {
 
     public static void main(String[] args) {
-        Duck duck = new Duck(new SimpleQuack(), new DisplayAsText(), new SimpleFly());
-        duck.quack();
-        duck.display();
-        duck.fly();
+        WeatherStation weatherStation = new WeatherStation();
+        PhoneDisplay display1 = new PhoneDisplay(weatherStation);
+        PhoneDisplay display2 = new PhoneDisplay(weatherStation);
+        WindowDisplay display3 = new WindowDisplay(weatherStation);
+
+        weatherStation.register(display1);
+        weatherStation.register(display2);
+        weatherStation.register(display3);
     }
 }
